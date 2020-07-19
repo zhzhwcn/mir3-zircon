f52e80c393fee44443c156b655e51d9848af5930
zhzhwcn <zhzhwmail@gmail.com>
2020-07-19 13:57:10 +0800

    monster and drop import OK

--- Server/Views/MonsterInfoView.Designer.cs
+++ Server/Views/MonsterInfoView.Designer.cs
@@ -53,6 +53,7 @@
             this.colDAmount = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn6 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn7 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.MonsterInfoGridView = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.colMonsterName = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colImage = new DevExpress.XtraGrid.Columns.GridColumn();
@@ -73,7 +74,7 @@
             this.SaveButton = new DevExpress.XtraBars.BarButtonItem();
             this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
             this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
-            this.gridColumn7 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.ImportBtn = new DevExpress.XtraBars.BarButtonItem();
             ((System.ComponentModel.ISupportInitialize)(this.MonsterInfoStatsGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.StatComboBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.MonsterInfoGridControl)).BeginInit();
@@ -91,6 +92,7 @@
             this.MonsterInfoStatsGridView.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
             this.colStat,
             this.colAmount});
+            this.MonsterInfoStatsGridView.DetailHeight = 377;
             this.MonsterInfoStatsGridView.GridControl = this.MonsterInfoGridControl;
             this.MonsterInfoStatsGridView.Name = "MonsterInfoStatsGridView";
             this.MonsterInfoStatsGridView.OptionsView.EnableAppearanceEvenRow = true;
@@ -103,9 +105,11 @@
             // 
             this.colStat.ColumnEdit = this.StatComboBox;
             this.colStat.FieldName = "Stat";
+            this.colStat.MinWidth = 23;
             this.colStat.Name = "colStat";
             this.colStat.Visible = true;
             this.colStat.VisibleIndex = 0;
+            this.colStat.Width = 87;
             // 
             // StatComboBox
             // 
@@ -117,9 +121,11 @@
             // colAmount
             // 
             this.colAmount.FieldName = "Amount";
+            this.colAmount.MinWidth = 23;
             this.colAmount.Name = "colAmount";
             this.colAmount.Visible = true;
             this.colAmount.VisibleIndex = 1;
+            this.colAmount.Width = 87;
             // 
             // MonsterInfoGridControl
             // 
@@ -134,7 +140,7 @@
             gridLevelNode1,
             gridLevelNode2,
             gridLevelNode3});
-            this.MonsterInfoGridControl.Location = new System.Drawing.Point(0, 143);
+            this.MonsterInfoGridControl.Location = new System.Drawing.Point(0, 147);
             this.MonsterInfoGridControl.MainView = this.MonsterInfoGridView;
             this.MonsterInfoGridControl.MenuManager = this.ribbon;
             this.MonsterInfoGridControl.Name = "MonsterInfoGridControl";
@@ -144,7 +150,7 @@
             this.ItemLookUpEdit,
             this.MapLookUpEdit});
             this.MonsterInfoGridControl.ShowOnlyPredefinedDetails = true;
-            this.MonsterInfoGridControl.Size = new System.Drawing.Size(775, 374);
+            this.MonsterInfoGridControl.Size = new System.Drawing.Size(904, 410);
             this.MonsterInfoGridControl.TabIndex = 2;
             this.MonsterInfoGridControl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.RespawnsGridView,
@@ -162,6 +168,7 @@
             this.colSpread,
             this.colCount,
             this.gridColumn2});
+            this.RespawnsGridView.DetailHeight = 377;
             this.RespawnsGridView.GridControl = this.MonsterInfoGridControl;
             this.RespawnsGridView.Name = "RespawnsGridView";
             this.RespawnsGridView.OptionsView.EnableAppearanceEvenRow = true;
@@ -174,9 +181,11 @@
             // 
             this.colMap.ColumnEdit = this.MapLookUpEdit;
             this.colMap.FieldName = "Map";
+            this.colMap.MinWidth = 23;
             this.colMap.Name = "colMap";
             this.colMap.Visible = true;
             this.colMap.VisibleIndex = 0;
+            this.colMap.Width = 87;
             // 
             // MapLookUpEdit
             // 
@@ -195,44 +204,56 @@
             // colX
             // 
             this.colX.FieldName = "X";
+            this.colX.MinWidth = 23;
             this.colX.Name = "colX";
             this.colX.Visible = true;
             this.colX.VisibleIndex = 1;
+            this.colX.Width = 87;
             // 
             // colY
             // 
             this.colY.FieldName = "Y";
+            this.colY.MinWidth = 23;
             this.colY.Name = "colY";
             this.colY.Visible = true;
             this.colY.VisibleIndex = 2;
+            this.colY.Width = 87;
             // 
             // colDelay
             // 
             this.colDelay.FieldName = "Delay";
+            this.colDelay.MinWidth = 23;
             this.colDelay.Name = "colDelay";
             this.colDelay.Visible = true;
             this.colDelay.VisibleIndex = 3;
+            this.colDelay.Width = 87;
             // 
             // colSpread
             // 
             this.colSpread.FieldName = "Spread";
+            this.colSpread.MinWidth = 23;
             this.colSpread.Name = "colSpread";
             this.colSpread.Visible = true;
             this.colSpread.VisibleIndex = 4;
+            this.colSpread.Width = 87;
             // 
             // colCount
             // 
             this.colCount.FieldName = "Count";
+            this.colCount.MinWidth = 23;
             this.colCount.Name = "colCount";
             this.colCount.Visible = true;
             this.colCount.VisibleIndex = 5;
+            this.colCount.Width = 87;
             // 
             // gridColumn2
             // 
             this.gridColumn2.FieldName = "DropSet";
+            this.gridColumn2.MinWidth = 23;
             this.gridColumn2.Name = "gridColumn2";
             this.gridColumn2.Visible = true;
             this.gridColumn2.VisibleIndex = 6;
+            this.gridColumn2.Width = 87;
             // 
             // DropsGridView
             // 
@@ -243,6 +264,7 @@
             this.gridColumn1,
             this.gridColumn6,
             this.gridColumn7});
+            this.DropsGridView.DetailHeight = 377;
             this.DropsGridView.GridControl = this.MonsterInfoGridControl;
             this.DropsGridView.Name = "DropsGridView";
             this.DropsGridView.OptionsView.EnableAppearanceEvenRow = true;
@@ -255,11 +277,13 @@
             // 
             this.ColItem.ColumnEdit = this.ItemLookUpEdit;
             this.ColItem.FieldName = "Item";
+            this.ColItem.MinWidth = 23;
             this.ColItem.Name = "ColItem";
             this.ColItem.OptionsColumn.AllowSort = DevExpress.Utils.DefaultBoolean.True;
             this.ColItem.SortMode = DevExpress.XtraGrid.ColumnSortMode.DisplayText;
             this.ColItem.Visible = true;
             this.ColItem.VisibleIndex = 0;
+            this.ColItem.Width = 87;
             // 
             // ItemLookUpEdit
             // 
@@ -280,30 +304,47 @@
             // colChance
             // 
             this.colChance.FieldName = "Chance";
+            this.colChance.MinWidth = 23;
             this.colChance.Name = "colChance";
             this.colChance.Visible = true;
             this.colChance.VisibleIndex = 1;
+            this.colChance.Width = 87;
             // 
             // colDAmount
             // 
             this.colDAmount.FieldName = "Amount";
+            this.colDAmount.MinWidth = 23;
             this.colDAmount.Name = "colDAmount";
             this.colDAmount.Visible = true;
             this.colDAmount.VisibleIndex = 2;
+            this.colDAmount.Width = 87;
             // 
             // gridColumn1
             // 
             this.gridColumn1.FieldName = "DropSet";
+            this.gridColumn1.MinWidth = 23;
             this.gridColumn1.Name = "gridColumn1";
             this.gridColumn1.Visible = true;
             this.gridColumn1.VisibleIndex = 3;
+            this.gridColumn1.Width = 87;
             // 
             // gridColumn6
             // 
             this.gridColumn6.FieldName = "PartOnly";
+            this.gridColumn6.MinWidth = 23;
             this.gridColumn6.Name = "gridColumn6";
             this.gridColumn6.Visible = true;
             this.gridColumn6.VisibleIndex = 4;
+            this.gridColumn6.Width = 87;
+            // 
+            // gridColumn7
+            // 
+            this.gridColumn7.FieldName = "EasterEvent";
+            this.gridColumn7.MinWidth = 23;
+            this.gridColumn7.Name = "gridColumn7";
+            this.gridColumn7.Visible = true;
+            this.gridColumn7.VisibleIndex = 5;
+            this.gridColumn7.Width = 87;
             // 
             // MonsterInfoGridView
             // 
@@ -322,6 +363,7 @@
             this.gridColumn3,
             this.gridColumn4,
             this.gridColumn5});
+            this.MonsterInfoGridView.DetailHeight = 377;
             this.MonsterInfoGridView.GridControl = this.MonsterInfoGridControl;
             this.MonsterInfoGridView.Name = "MonsterInfoGridView";
             this.MonsterInfoGridView.OptionsDetail.AllowExpandEmptyDetails = true;
@@ -334,17 +376,21 @@
             // colMonsterName
             // 
             this.colMonsterName.FieldName = "MonsterName";
+            this.colMonsterName.MinWidth = 23;
             this.colMonsterName.Name = "colMonsterName";
             this.colMonsterName.Visible = true;
             this.colMonsterName.VisibleIndex = 0;
+            this.colMonsterName.Width = 87;
             // 
             // colImage
             // 
             this.colImage.ColumnEdit = this.MonsterImageComboBox;
             this.colImage.FieldName = "Image";
+            this.colImage.MinWidth = 23;
             this.colImage.Name = "colImage";
             this.colImage.Visible = true;
             this.colImage.VisibleIndex = 1;
+            this.colImage.Width = 87;
             // 
             // MonsterImageComboBox
             // 
@@ -356,106 +402,131 @@
             // colAI
             // 
             this.colAI.FieldName = "AI";
+            this.colAI.MinWidth = 23;
             this.colAI.Name = "colAI";
             this.colAI.Visible = true;
             this.colAI.VisibleIndex = 2;
+            this.colAI.Width = 87;
             // 
             // colLevel
             // 
             this.colLevel.FieldName = "Level";
+            this.colLevel.MinWidth = 23;
             this.colLevel.Name = "colLevel";
             this.colLevel.Visible = true;
             this.colLevel.VisibleIndex = 3;
+            this.colLevel.Width = 87;
             // 
             // colExperience
             // 
             this.colExperience.FieldName = "Experience";
+            this.colExperience.MinWidth = 23;
             this.colExperience.Name = "colExperience";
             this.colExperience.Visible = true;
             this.colExperience.VisibleIndex = 4;
+            this.colExperience.Width = 87;
             // 
             // colViewRange
             // 
             this.colViewRange.FieldName = "ViewRange";
+            this.colViewRange.MinWidth = 23;
             this.colViewRange.Name = "colViewRange";
             this.colViewRange.Visible = true;
             this.colViewRange.VisibleIndex = 5;
+            this.colViewRange.Width = 87;
             // 
             // colCoolEye
             // 
             this.colCoolEye.FieldName = "CoolEye";
+            this.colCoolEye.MinWidth = 23;
             this.colCoolEye.Name = "colCoolEye";
             this.colCoolEye.Visible = true;
             this.colCoolEye.VisibleIndex = 6;
+            this.colCoolEye.Width = 87;
             // 
             // colAttackDelay
             // 
             this.colAttackDelay.FieldName = "AttackDelay";
+            this.colAttackDelay.MinWidth = 23;
             this.colAttackDelay.Name = "colAttackDelay";
             this.colAttackDelay.Visible = true;
             this.colAttackDelay.VisibleIndex = 7;
+            this.colAttackDelay.Width = 87;
             // 
             // colMoveDelay
             // 
             this.colMoveDelay.FieldName = "MoveDelay";
+            this.colMoveDelay.MinWidth = 23;
             this.colMoveDelay.Name = "colMoveDelay";
             this.colMoveDelay.Visible = true;
             this.colMoveDelay.VisibleIndex = 8;
+            this.colMoveDelay.Width = 87;
             // 
             // colIsBoss
             // 
             this.colIsBoss.FieldName = "IsBoss";
+            this.colIsBoss.MinWidth = 23;
             this.colIsBoss.Name = "colIsBoss";
             this.colIsBoss.Visible = true;
             this.colIsBoss.VisibleIndex = 9;
+            this.colIsBoss.Width = 87;
             // 
             // colUndead
             // 
             this.colUndead.FieldName = "Undead";
+            this.colUndead.MinWidth = 23;
             this.colUndead.Name = "colUndead";
             this.colUndead.Visible = true;
             this.colUndead.VisibleIndex = 10;
+            this.colUndead.Width = 87;
             // 
             // gridColumn3
             // 
             this.gridColumn3.FieldName = "CanPush";
+            this.gridColumn3.MinWidth = 23;
             this.gridColumn3.Name = "gridColumn3";
             this.gridColumn3.Visible = true;
             this.gridColumn3.VisibleIndex = 11;
+            this.gridColumn3.Width = 87;
             // 
             // gridColumn4
             // 
             this.gridColumn4.FieldName = "CanTame";
+            this.gridColumn4.MinWidth = 23;
             this.gridColumn4.Name = "gridColumn4";
             this.gridColumn4.Visible = true;
             this.gridColumn4.VisibleIndex = 12;
+            this.gridColumn4.Width = 87;
             // 
             // gridColumn5
             // 
             this.gridColumn5.FieldName = "Flag";
+            this.gridColumn5.MinWidth = 23;
             this.gridColumn5.Name = "gridColumn5";
             this.gridColumn5.Visible = true;
             this.gridColumn5.VisibleIndex = 13;
+            this.gridColumn5.Width = 87;
             // 
             // ribbon
             // 
             this.ribbon.ExpandCollapseItem.Id = 0;
             this.ribbon.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
             this.ribbon.ExpandCollapseItem,
-            this.SaveButton});
+            this.SaveButton,
+            this.ImportBtn});
             this.ribbon.Location = new System.Drawing.Point(0, 0);
-            this.ribbon.MaxItemId = 2;
+            this.ribbon.MaxItemId = 3;
             this.ribbon.Name = "ribbon";
             this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.ribbonPage1});
-            this.ribbon.Size = new System.Drawing.Size(775, 143);
+            this.ribbon.Size = new System.Drawing.Size(904, 147);
             // 
             // SaveButton
             // 
             this.SaveButton.Caption = "Save Database";
-            this.SaveButton.Glyph = ((System.Drawing.Image)(resources.GetObject("SaveButton.Glyph")));
             this.SaveButton.Id = 1;
-            this.SaveButton.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("SaveButton.LargeGlyph")));
+            this.SaveButton.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("SaveButton.ImageOptions.Image")));
+            this.SaveButton.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("SaveButton.ImageOptions.LargeImage")));
             this.SaveButton.LargeWidth = 60;
             this.SaveButton.Name = "SaveButton";
             this.SaveButton.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.SaveButton_ItemClick);
@@ -471,22 +542,23 @@
             // 
             this.ribbonPageGroup1.AllowTextClipping = false;
             this.ribbonPageGroup1.ItemLinks.Add(this.SaveButton);
+            this.ribbonPageGroup1.ItemLinks.Add(this.ImportBtn);
             this.ribbonPageGroup1.Name = "ribbonPageGroup1";
             this.ribbonPageGroup1.ShowCaptionButton = false;
             this.ribbonPageGroup1.Text = "Saving";
             // 
-            // gridColumn7
+            // ImportBtn
             // 
-            this.gridColumn7.FieldName = "EasterEvent";
-            this.gridColumn7.Name = "gridColumn7";
-            this.gridColumn7.Visible = true;
-            this.gridColumn7.VisibleIndex = 5;
+            this.ImportBtn.Caption = "Import";
+            this.ImportBtn.Id = 2;
+            this.ImportBtn.Name = "ImportBtn";
+            this.ImportBtn.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.ImportBtn_ItemClick);
             // 
             // MonsterInfoView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(775, 517);
+            this.ClientSize = new System.Drawing.Size(904, 557);
             this.Controls.Add(this.MonsterInfoGridControl);
             this.Controls.Add(this.ribbon);
             this.Name = "MonsterInfoView";
@@ -551,5 +623,6 @@
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn6;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn7;
+        private DevExpress.XtraBars.BarButtonItem ImportBtn;
     }
 }
--- Server/Views/MonsterInfoView.cs
+++ Server/Views/MonsterInfoView.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CsvHelper;
 using DevExpress.XtraBars;
 using Library;
 using Library.SystemModels;
+using MirDB;
+using Server.Envir;
 
 namespace Server.Views
 {
@@ -33,6 +40,89 @@ namespace Server.Views
         private void SaveButton_ItemClick(object sender, ItemClickEventArgs e)
         {
             SMain.Session.Save(true);
+        }
+
+        private void ImportBtn_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var collection = SMain.Session.GetCollection<MonsterInfo>();
+            var dropCollection = SMain.Session.GetCollection<DropInfo>();
+            var itemCollection = SMain.Session.GetCollection<ItemInfo>();
+            using (var reader = new StreamReader("AmzData/Monster.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    var name = csv.GetField("Name");
+                    if (name.StartsWith("=") || collection.Binding.Any(m => m.MonsterName == name))
+                    {
+                        continue;
+                    }
+
+                    var monster = collection.CreateNewObject();
+                    monster.MonsterName = name;
+                    monster.Image = MonsterImage.Scarecrow;
+                    monster.AI = 0;
+                    monster.Level = csv.GetField<int>("Lvl");
+                    monster.ViewRange = 9;
+                    monster.CoolEye = csv.GetField<int>("CoolEye");
+                    monster.Experience = csv.GetField<int>("Exp");
+                    monster.Undead = csv.GetField<bool>("Undead");
+                    monster.CanPush = true;
+                    monster.CanTame = true;
+                    monster.IsBoss = false;
+                    monster.Flag = MonsterFlag.None;
+
+                    monster.AttackDelay = csv.GetField<int>("ATTACK_SPD");
+                    monster.MoveDelay = csv.GetField<int>("WALK_SPD");
+                    if (File.Exists($"AmzData/MonItems/{name}.txt"))
+                    {
+                        var dropList = File.ReadAllLines($"AmzData/MonItems/{name}.txt", Encoding.GetEncoding("GB2312"));
+                        for (var index = 0; index < dropList.Length; index++)
+                        {
+                            var s = dropList[index];
+                            if (string.IsNullOrEmpty(s)) continue;
+                            if (s.StartsWith("#")) continue;
+                            if (s.StartsWith("("))
+                            {
+                                while (s != ")")
+                                {
+                                    s = dropList[index++];
+                                }
+
+                                s = dropList[index];
+                            }
+
+                            var info = s.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+                            if(info.Length < 2) continue;
+                            var item = itemCollection.Binding.FirstOrDefault(i => i.ItemName == info[1]);
+                            if (item == null)
+                            {
+                                SEnvir.Log($"item not found: {info[1]}");
+                                continue;
+                            }
+                            var drop = dropCollection.CreateNewObject();
+                            drop.Monster = monster;
+                            drop.Item = item;
+                            if (info.Length == 3)
+                            {
+                                drop.Amount = int.Parse(info[2]);
+                            }
+                            else
+                            {
+                                drop.Amount = 1;
+                            }
+
+                            drop.Chance = int.Parse(info[0].Replace("1/", ""));
+                            drop.DropSet = 0;
+                            drop.PartOnly = false;
+                            drop.EasterEvent = false;
+                            monster.Drops.Add(drop);
+                        }
+                    }
+                }
+            }
         }
     }
 }
